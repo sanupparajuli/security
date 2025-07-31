f7f1a527b02d6fb3b7217fdd2b885323d8c36a0b
sanupparajuli <112367579+sanupparajuli@users.noreply.github.com>
2025-07-31 20:43:14 +0545

    initial commit

--- models/gig.model.js
+++ models/gig.model.js
@@ -15,7 +15,9 @@ const GigSchema = new Schema(
       type: String,
       required: true,
     },
-    cat:
+    cat:{
+      type: String,
+      required: true
     },
     desc: {
       type: String,
