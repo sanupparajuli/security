8c34983c84f743e9c5e072d87e2e1f64c15c1fa3
sanupparajuli <112367579+sanupparajuli@users.noreply.github.com>
2025-07-31 20:36:22 +0545

    initial commit

--- models/gig.model.js
+++ models/gig.model.js
@@ -3,6 +3,17 @@ const { Schema } = mongoose;
 
 const GigSchema = new Schema(
   {
+    userId: {
+      type: String,
+      required: true
+    },
+    sellerUsername: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
     },
     cat:{
       type: String,
